3e56782a9e56a27134728851c5a5e5bf478aae9c
Pushkar Prajapati <kppusakar2018@gmail.com>
2026-01-24 12:08:59 +0530

    Update README with project description
    Added a detailed description of the repository's purpose and content.

---
+++ src/loops/FirstProgram.java
@@ -0,0 +1,7 @@
+package loops;
+
+public class FirstProgram {
+    public static void main(String[] args) {
+        System.out.println("Hi, this is your first program in DSA with java.");
+    }
+}
